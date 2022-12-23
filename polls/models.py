6a5bf3e01f562cf4f5ba1c4f06ab3c1b75b7e570
VladislavAlpeev <vlad.alpeev@mail.ru>
2022-12-23 03:50:48 +0300

    added old project files

---
+++ polls/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+class Country(models.Model):
+    name = models.CharField(max_length=200)
+    population = models.IntegerField()
+    area = models.IntegerField()
+    image = models.ImageField()
---
+++ polls/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+from .models import Country
+
+def index(request):
+    countries_list = Country.objects.order_by('-area')[:3]
+    context = { 
+        'countries_list': countries_list
+    }
+    return render(request, "polls/index.html", context)
+
+def detail(request, id):
+    certain_country = Country.objects.filter(id=id).first()
+    context = {
+        'certain_country': certain_country
+    }
+    return render(request, "polls/detail.html", context)
+
